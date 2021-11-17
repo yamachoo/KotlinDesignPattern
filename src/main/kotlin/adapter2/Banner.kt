7339227f5f37aae8ef922a2b602b30f13e1b4048
yamachoo <51016095+yamachoo@users.noreply.github.com>
2021-11-17 22:10:25 +0900

    clean ktlint format

--- src/main/kotlin/adapter2/Banner.kt
+++ src/main/kotlin/adapter2/Banner.kt
@@ -1,6 +1,6 @@
 package adapter2
 
 open class Banner(private val string: String) {
-    fun showWithParen() = println("(${string})")
-    fun showWithAster() = println("*${string}*")
+    fun showWithParen() = println("($string)")
+    fun showWithAster() = println("*$string*")
 }
--- src/main/kotlin/templateMethod/StringDisplay.kt
+++ src/main/kotlin/templateMethod/StringDisplay.kt
@@ -4,7 +4,7 @@ class StringDisplay(private val string: String) : AbstractDisplay() {
     private val width = string.toByteArray().size
 
     override fun open() = printLine()
-    override fun print() = println("|${string}|")
+    override fun print() = println("|$string|")
     override fun close() = printLine()
     private fun printLine() {
         print("+")
