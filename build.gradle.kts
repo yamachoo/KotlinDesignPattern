41d628415c0f83a3f53e4084ae6c80593e0c630f
yamachoo <51016095+yamachoo@users.noreply.github.com>
2021-11-17 22:08:16 +0900

    add ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.5.31"
+    id("org.jlleitschuh.gradle.ktlint") version "10.2.0"
 }
 
 group = "me.ryota"
